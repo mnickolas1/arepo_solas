d2a8e40b410ab64c79a1e3651165e8ad63eb7a6d
Nick <mnickolas.1@gmail.com>
2025-08-20 11:46:11 +1000

    Cleaning up kernels

--- src/main/update.c
+++ src/main/update.c
@@ -11,91 +11,16 @@
 #include "../../celib/src/config.h"
 
 #if defined(STARS) || defined(BLACKHOLES)
-/* THIS PART ADAPTED FROM GADGET4 */
-/* fall back to cubic spline kernel */
-#if !defined(CUBIC_SPLINE_KERNEL) && !defined(WENDLAND_C2_KERNEL) && !defined(WENDLAND_C4_KERNEL) && !defined(WENDLAND_C6_KERNEL)
-#define CUBIC_SPLINE_KERNEL 
-#endif
-
-/* fall back to three dimensions */
-#if !defined(TWODIMS) && !defined(ONEDIMS)
-#define THREEDIMS
-#endif
-
-/* Norms */
-#ifdef CUBIC_SPLINE_KERNEL
-
-#ifdef THREEDIMS
-#define K_NORM (8.0 / M_PI) /*!< For 3D-normalized kernel */
-#endif
-
-#ifdef TWODIMS
-#define K_NORM (40.0 / (7.0 * M_PI)) /*!< For 2D-normalized kernel */
-#endif
-
-#ifdef ONEDIMS
-#define K_NORM (4.0 / 3.0) /*!< For 1D-normalized kernel */
-#endif
-
-#endif /* CUBIC_SPLINE_KERNEL */
-
-#ifdef WENDLAND_C2_KERNEL
-
-#ifdef THREEDIMS
-#define K_NORM (21.0 / (2.0 * M_PI)) /*!< For 3D-normalized kernel */
-#endif
-
-#ifdef TWODIMS
-#define K_NORM (7.0 / M_PI) /*!< For 2D-normalized kernel */
-#endif
-
-#ifdef ONEDIMS
-#define K_NORM (5.0 / 4.0) /*!< For 1D-normalized kernel */
-#endif
-
-#endif /* WENDLAND_C2_KERNEL */
-
-#ifdef WENDLAND_C4_KERNEL
-
-#ifdef THREEDIMS
-#define K_NORM (495.0 / (32.0 * M_PI)) /*!< For 3D-normalized kernel */
-#endif
-
-#ifdef TWODIMS
-#define K_NORM (9.0 / M_PI) /*!< For 2D-normalized kernel */
-#endif
-
-#ifdef ONEDIMS
-#define K_NORM (3.0 / 2.0) /*!< For 1D-normalized kernel */
-#endif
-
-#endif /* WENDLAND_C4_KERNEL */
-
-#ifdef WENDLAND_C6_KERNEL
-
-#ifdef THREEDIMS
-#define K_NORM (1365.0 / (64.0 * M_PI)) /*!< For 3D-normalized kernel */
-#endif
-
-#ifdef TWODIMS
-#define K_NORM (78.0 / (7.0 * M_PI)) /*!< For 2D-normalized kernel */
-#endif
-
-#ifdef ONEDIMS
-#define K_NORM (55.0 / 32.0) /*!< For 1D-normalized kernel */
-#endif
-
-#endif /* WENDLAND_C6_KERNEL */
 
 static int int_compare(const void *a, const void *b);
 
-/* sph loop kernel function -> u < 1 */
+/*sph loop kernel function*/
 void kernel(double u, double hinv3, double hinv4, double *wk, double *dwk)
 {
-#ifdef CUBIC_SPLINE_KERNEL
-#if defined(WENDLAND_C2_KERNEL) || defined(WENDLAND_C4_KERNEL) || defined(WENDLAND_C6_KERNEL)
-#error "Only one SPH kernel can be used"
-#endif
+/*cubic spline*/
+
+  double K_norm = 8.0 / M_PI;
+
   if(u < 0.5)
     {
       *dwk = u * (18.0 * u - 12.0);
@@ -111,86 +36,12 @@ void kernel(double u, double hinv3, double hinv4, double *wk, double *dwk)
       
       *wk = 2.0 * t2 * t1;
     }
-#endif
 
-#ifdef WENDLAND_C2_KERNEL /* Dehnen & Aly 2012 */
-#ifdef ONEDIMS
-  double t1 = (1.0 - u);
-  double t2 = (t1 * t1);
-
+  *dwk *= K_norm * hinv4;
   
-  *dwk = -12.0 * u * t2;
-  
-  *wk = t2 * t1 * (1.0 + u * 3.0);
-
-#else /* 2d or 3d */
-  double t1 = (1.0 - u);
-  double t2 = (t1 * t1);
-  double t4 = t2 * t2;
-  
-  *dwk = -20.0 * u * t2 * t1;
-  
-  *wk = t4 * (1.0 + u * 4.0);
-
-#endif
-#endif /* WENDLAND_C2_KERNEL */
-
-#ifdef WENDLAND_C4_KERNEL /* Dehnen & Aly 2012 */
-#ifdef ONEDIMS
-  double t1 = (1.0 - u);
-  double t2 = t1 * t1;
-  double t4 = t2 * t2;
-  double t5 = t4 * t1;
-
-  
-  *dwk = -14.0 * t4 * (4.0 * u + 1) * u;
-  
-  *wk = t5 * (1.0 + u * (5.0 + 8.0 * u));
-
-#else /* 2d or 3d */
-  double t1 = (1.0 - u);
-  double t2 = (t1 * t1);
-  double t4 = t2 * t2;
-  double t6 = t2 * t2 * t2;
-  
-  *dwk = -56.0 / 3.0 * u * t4 * t1 * (5.0 * u + 1);
-  
-  *wk = t6 * (1.0 + u * (6.0 + 35.0 / 3.0 * u));
-
-#endif
-#endif /* WENDLAND_C4_KERNEL */
-
-#ifdef WENDLAND_C6_KERNEL /* Dehnen & Aly 2012 */
-#ifdef ONEDIMS
-  double t1 = (1.0 - u);
-  double t2 = (t1 * t1);
-  double t4 = t2 * t2;
-  double t6 = t4 * t2;
-  double t7 = t4 * t2 * t1;
-  
-  *dwk = -6.0 * u * t6 * (3.0 + u * (18.0 + 35.0 * u));
-  
-  *wk = t7 * (1.0 + u * (7.0 + u * (19.0 + 21.0 * u)));
-
-#else /* 2d or 3d */
-  double t1 = (1.0 - u);
-  double t2 = (t1 * t1);
-  double t4 = t2 * t2;
-  double t7 = t4 * t2 * t1;
-  double t8 = t4 * t4;
-  
-  *dwk = -22.0 * u * (1.0 + u * (7.0 + 16.0 * u)) * t7;
-  
-  *wk = t8 * (1.0 + u * (8.0 + u * (25.0 + 32.0 * u)));
-
-#endif
-#endif /* WENDLAND_C6_KERNEL */
-  
-  *dwk *= K_NORM * hinv4;
-  
-  *wk *= K_NORM * hinv3;
+  *wk  *= K_norm * hinv3;
 }
-/* THIS PART ADAPTED FROM GADGET4 */
+
 
 #ifdef BLACKHOLES
 #ifdef BONDI_ACCRETION 
