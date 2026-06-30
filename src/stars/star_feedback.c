24afafa0bcd82e79b80a47c815c7a23fb7bd429f
Nick Maragkakis <mnickolas.1@gmail.com>
2026-06-30 13:07:00 +0800

    Change max export for star_feedback to scale with All.TotNumGas

--- src/stars/star_feedback.c
+++ src/stars/star_feedback.c
@@ -151,7 +151,7 @@ void star_feedback(void)
   int ev, h, i, k, q, f;
 
   int n_export = 0;
-  int max_export = 100 * NTask;
+  int max_export = 20 * All.TotNumGas;
   int *ExportTask = mymalloc("ExportTask", max_export * sizeof(int));
   struct Feedback_Kick *ExportBuf = mymalloc("ExportBuf",  max_export * sizeof(struct Feedback_Kick));
 
