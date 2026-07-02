29085f796181b12750ce618507e39f1ea1aa9630
Nick Maragkakis <mnickolas.1@gmail.com>
2026-07-02 08:22:17 +0800

    Change export kick allocation size to scale with TotNumStars

--- src/stars/star_feedback.c
+++ src/stars/star_feedback.c
@@ -151,7 +151,7 @@ void star_feedback(void)
   int ev, h, i, k, q, f;
 
   int n_export = 0;
-  int max_export = 20 * All.TotNumGas;
+  int max_export = 20 * All.TotNumStars;
   int *ExportTask = mymalloc("ExportTask", max_export * sizeof(int));
   struct Feedback_Kick *ExportBuf = mymalloc("ExportBuf",  max_export * sizeof(struct Feedback_Kick));
 
